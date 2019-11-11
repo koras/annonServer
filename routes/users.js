ded4a025ffb9b8f6609846bc72b0cf992bc6b1de
koras <kos6476@yandex.ru>
2019-11-12 00:11:07 +0300

    correct errors

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,17 @@ const auth = require('../middlewares/auth');
 
 const {
   createUser,
-  login,
-  getUser,
-  getUsers,
-  patchAcc,
+  //  login,
+  // getUser,
+  // getUsers,
+  // patchAcc,
 } = require('../controllers/users');
 
 router.post('/users/create', createUser);
-router.post('/signin', login);
+//router.post('/signin', login);
 
-router.get('/users', auth, getUsers);
-router.get('/users/:id', auth, getUser);
-router.patch('/users/me', auth, patchAcc);
+// router.get('/users', auth, getUsers);
+// router.get('/users/:id', auth, getUser);
+// router.patch('/users/me', auth, patchAcc);
 
 module.exports = router;
